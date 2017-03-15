2e5567782ceb3f96b778c4ea69d1f4b70e64cdf0
James Dawson <james@readsource.co.uk>
2017-03-15 15:58:23 +0000

    Started elasticsearch spike

---
+++ _elk/run.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=c:\Program Files\Java\jre1.8.0_121
+start %~dp0elasticsearch-5.2.2\bin\elasticsearch.bat
+start %~dp0kibana-5.2.2-windows-x86\bin\kibana.bat
---
+++ _elk/setup.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = "Stop"
+$here = Split-Path $PSCommandPath -Parent
+
+try {
+	Invoke-RestMethod -Method DELETE -Uri http://localhost:9200/thedivision
+}
+catch {
+}
+
+Invoke-RestMethod -Method PUT -Uri http://localhost:9200/thedivision
+Invoke-RestMethod -Method PUT -Uri http://localhost:9200/thedivision/item/_mapping -Body (gc -raw $here\item.json)
+Invoke-RestMethod -Method PUT -Uri http://localhost:9200/thedivision/build_info/_mapping -Body (gc -raw $here\build_info.json)
+
+$items = ConvertFrom-Json (gc -raw "$here\..\my-gear-list-with-mods.json")
+foreach ($item in $items)
+{
+	$itemAsJson = $item | ConvertTo-Json -Compress
+	Invoke-Restmethod -Method POST -Uri http://localhost:9200/thedivision/item -Body $itemAsJson
+}
